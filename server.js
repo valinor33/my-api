d8e2490feef0b5f7e4266aaa46495d92be6e0a0b
valinor <andresfullstack@gmail.com>
2021-03-12 19:20:32 -0300

    update

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 const path = require("path");
 
 /* PORT */
-const PORT = process.env.APP_PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 const valinorRouter = require("./router/valinorRouter");
 const morgan = require("morgan");
